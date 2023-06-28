ecd3b9bb5f655396289c16ec564ba172c2bd5baa
Semih Bag <semih702baqmail.com>
2023-06-28 10:51:29 +0300

    update

---
+++ src/_1218_Longest_Arithmetic_Subsequence_of_Given_Difference.java
@@ -0,0 +1,13 @@
+
+public class _1218_Longest_Arithmetic_Subsequence_of_Given_Difference {
+	  public int longestSubsequence(int[] arr, int difference) {
+		  int tail;
+		  int k = 0;
+		  int len = arr.length;
+	
+		  for (int i = len -1; i <= 0; i--) {
+			  
+		  }
+	    }
+	
+}
--- src/_2293_Min_Max_Game.java
+++ src/_2293_Min_Max_Game.java
@@ -1,4 +1,25 @@
-
 public class _2293_Min_Max_Game {
-
+    public int minMaxGame(int[] nums) {
+        int n = nums.length;
+    	if (n == 1) {
+    		return nums[0];
+    	}
+    	
+    	int[] newNums = new int[n/2];
+    	
+    	for (int i = 0; i < n/2; i++) {
+			int i_ = i*2;
+			int a;
+			
+    		if (i%2 ==0) {
+    			a = (nums[i_] < nums[i_ +1])? nums[i_] : nums[i_ +1];
+    		}
+    		else {
+    			a = (nums[i_] > nums[i_ +1])? nums[i_] : nums[i_ +1];
+    		}
+			newNums[i] = a;
+    	}
+    
+    return minMaxGame(newNums);
+    }
 }
